d81be75679e1eae26da2e4aa7a104bc4827ad06f
klihonga11 <klihonga11@gmail.com>
2026-01-01 10:04:15 +0200

    Update files

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { system } from './theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
