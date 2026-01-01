88a5c924abf192af0875f474c94c475c9f8e515c
klihonga11 <klihonga11@gmail.com>
2026-01-01 09:47:16 +0200

    setup

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,11 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   globalIgnores(['dist']),
+  prettier,
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
