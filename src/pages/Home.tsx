0007d3be23a66af60bd18224e8f12a629d1b3919
klihonga11 <klihonga11@gmail.com>
2026-01-04 07:01:47 +0200

    Update card layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,63 @@
-import { Text } from '@chakra-ui/react';
+import { ProgressCircle, Text } from '@chakra-ui/react';
 import useFetch from '../hooks/useFetch';
 import { Card } from '@chakra-ui/react';
+import type { JokeData } from '../types/jokedata';
 
 export default function Home() {
-  const { loading, error, data } = useFetch('https://v2.jokeapi.dev/joke/Any');
+  const { loading, error, data } = useFetch(
+    'https://v2.jokeapi.dev/joke/Any?type=single'
+  );
 
-  if (loading) {
-    return <Text>Is Loading</Text>;
-  }
+  if (loading) return <LoadingIndicator />;
+  if (!data) return <ErrorState message={error} />;
 
-  if (error) {
-    return <Text>Error: {error}</Text>;
-  }
+  return <SuccessState data={data} />;
+}
 
+type ErrorProps = {
+  message: string;
+};
+
+function ErrorState({ message }: ErrorProps) {
   return (
     <>
       <Card.Root>
         <Card.Header>
-          <Text>{data?.category}</Text>
+          <Text>Error</Text>
         </Card.Header>
         <Card.Body>
-          <Text>{data?.joke}</Text>
+          <Text>{message}</Text>
         </Card.Body>
-        <Card.Footer />
       </Card.Root>
     </>
+  );
+}
+
+type SuccessProps = {
+  data: JokeData;
+};
+
+function SuccessState({ data }: SuccessProps) {
+  return (
+    <Card.Root>
+      <Card.Header>
+        <Text>{data.category}</Text>
+      </Card.Header>
+      <Card.Body>
+        <Text>{data.joke}</Text>
+      </Card.Body>
+    </Card.Root>
+  );
+}
+
+function LoadingIndicator() {
+  return (
+    <ProgressCircle.Root value={null}>
+      <ProgressCircle.Circle>
+        <ProgressCircle.Track />
+        <ProgressCircle.Range />
+      </ProgressCircle.Circle>
+      <ProgressCircle.ValueText />
+    </ProgressCircle.Root>
   );
 }
