b09d436eb332830d3542ee32f1645bf47a30deda
klihonga11 <klihonga11@gmail.com>
2026-01-05 10:29:16 +0200

    Create nav bar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from '@chakra-ui/react';
+
+export default function NavBar() {
+  return (
+    <Box bg="blue" color="white" padding={4}>
+      <Text>My dashboard</Text>
+    </Box>
+  );
+}
