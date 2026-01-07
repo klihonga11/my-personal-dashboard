822e9b6c9116037947e218d8ab60d06e44b571ef
klihonga11 <klihonga11@gmail.com>
2026-01-07 10:45:32 +0200

    Configure router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+import JokesPage from './pages/Jokes';
+import Pages from './layout/Pages';
+import WeatherPage from './pages/Weather';
+import WelcomePage from './pages/Welcome';
 
 export default function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route element={<Pages />}>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/jokes" element={<JokesPage />} />
+        <Route path="/weather" element={<WeatherPage />} />
+      </Route>
+    </Routes>
+  );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,14 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, HStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
     <Box bg="blue" color="white" padding={4}>
-      <Text>My dashboard</Text>
+      <HStack>
+        <Link to="/">Home</Link>
+        <Link to="/jokes">Jokes</Link>
+        <Link to="/weather">Weather</Link>
+      </HStack>
     </Box>
   );
 }
